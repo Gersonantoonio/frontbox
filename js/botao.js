afc358867c77372483cab7861f448260d4a102fa
Gerson António <malungoferraz@gmail.com>
2023-01-08 16:42:00 +0100

    versao

---
+++ js/botao.js
@@ -0,0 +1,19 @@
+// inicio da funcao do menu
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('active');
+    const active = menu.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+//fim da funcao do menu
